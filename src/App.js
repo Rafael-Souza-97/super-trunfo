8e9994eb2944680f35b67fb15ba7d0b2685f992b
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-25 21:49:53 -0300

    Adiciona Requisito 5

--- src/App.js
+++ src/App.js
@@ -19,11 +19,45 @@ class App extends React.Component {
     };
   }
 
+  validateButton = (() => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const maxAttrNumber = 90;
+    const sumAttrNumber = 210;
+
+    const numCardAttr1 = parseInt(cardAttr1, 10);
+    const numCardAttr2 = parseInt(cardAttr2, 10);
+    const numCardAttr3 = parseInt(cardAttr3, 10);
+
+    const verfifySum = numCardAttr1 + numCardAttr2 + numCardAttr3 <= sumAttrNumber;
+    const verifyMaxAttr = numCardAttr1 <= maxAttrNumber
+    && numCardAttr2 <= maxAttrNumber
+    && numCardAttr3 <= maxAttrNumber;
+    const verifyMinAttr = cardAttr1 >= 0 && cardAttr2 >= 0 && cardAttr3 >= 0;
+    const verifyed = verfifySum && verifyMaxAttr && verifyMinAttr;
+
+    if (cardName.length > 0
+      && cardDescription.length > 0
+      && cardImage.length > 0
+      && verifyed) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  });
+
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, this.validateButton);
   }
 
   render() {
